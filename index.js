67b906cba290e2a85c91e4890bda3899892cc3fe
Jolyon Saunders <jol.saund@gmail.com>
2020-05-03 14:40:39 +1000

    Created simple benchmark to compare all implementations

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-const rustAdddon = require('./rust-addon')
+const rustAddon = require('./rust-addon')
 const wasmPack = require('./wasm-pack/pkg')
 const neon = require('./neon')
 const napiRs = require('./napi-rs-addon')
 const nodeBindgen = require('./node-bindgen-addon')
 
-console.log(rustAdddon.add(2, 3))
-console.log(wasmPack.add(2, 3))
-console.log(neon.add(2, 3))
-console.log(napiRs.add(2, 3))
-console.log(nodeBindgen.add(2, 3))
+const NS_PER_SEC = 1e9
+
+const modules = {
+  rustAddon,
+  wasmPack,
+  neon,
+  napiRs,
+  nodeBindgen
+}
+
+const pairs = Array(1000000).fill().map(() => [Math.random(), Math.random()])
+
+function bench (func, jsTime) {
+  let totalTime = 0
+
+  for (const [a, b] of pairs) {
+    const start = process.hrtime()
+    func(a, b)
+    const diff = process.hrtime(start)
+    totalTime += diff[0] + (diff[1] / NS_PER_SEC)
+  }
+
+  return totalTime / jsTime
+}
+
+function add (a, b) {
+  return a + b
+}
+
+const jsTime = bench(add, 1)
+
+const results = { js: 1 }
+
+for (const mod of Object.keys(modules)) {
+  results[mod] = bench(modules[mod].add, jsTime)
+}
+
+console.log(results)
